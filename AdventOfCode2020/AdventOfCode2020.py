7b797501920cacc6c96373170e701eb978e0c624
Lachlan Kerr <lachlankerr96@gmail.com>
2020-12-04 15:37:32 +1100

    Add project files.

---
+++ AdventOfCode2020/AdventOfCode2020.py
@@ -0,0 +1,5 @@
+from day01 import read_input, part_one, part_two
+
+if __name__ == "__main__":
+    print(f"Day 01: {part_one(read_input())}")
+    print(f"Day 01: {part_two(read_input())}")
---
+++ AdventOfCode2020/day01.py
@@ -0,0 +1,55 @@
+'''
+    Day 01: https://adventofcode.com/2020/day/1
+'''
+
+magic_number = 2020
+
+def read_input():
+    '''
+        Reads the input file and returns a list of all the lines
+    '''
+
+    lines = []
+    with open('day01input.txt') as f:
+        lines = f.read().splitlines()
+
+    return lines
+
+def part_one(lines):
+    '''
+        Traverses the input lines and finds the two numbers that sum to 2020, then returns the product of them
+    '''
+
+    lines = list(map(int, lines)) # convert list to ints
+
+    lines.sort() # modifies in place
+
+    iterations = 0
+
+    for i in range(len(lines)):
+        for j in range(len(lines)):
+            iterations += 1
+            if lines[i] + lines[j] == magic_number:
+                return lines[i] * lines[j], iterations # currently 373 iterations
+            elif lines[i] + lines[j] > magic_number: # doesn't actually do anything for our list of numbers
+                break 
+
+def part_two(lines):
+    '''
+        Traverses the input lines and finds the three numbers that sum to 2020, then returns the product of them
+    '''
+
+    lines = list(map(int, lines)) # convert list to ints
+
+    lines.sort() # modifies in place
+
+    iterations = 0
+
+    for i in range(len(lines)):
+        for j in range(len(lines)):
+            for k in range(len(lines)):
+                iterations += 1
+                if lines[i] + lines[j] + lines[k] == magic_number:
+                    return lines[i] * lines[j] * lines[k], iterations # currently 4611 iterations
+                elif lines[i] + lines[j] + lines[k] > magic_number: # reduces iterations by 76446
+                    break 
