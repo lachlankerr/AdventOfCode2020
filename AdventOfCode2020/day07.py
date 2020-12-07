732a8fb13390ef34c24878d459df50194cfa1ba3
Lachlan Kerr <lachlankerr96@gmail.com>
2020-12-07 21:08:21 +1100

    Finish day 07

--- AdventOfCode2020/day07.py
+++ AdventOfCode2020/day07.py
@@ -55,4 +55,35 @@ def part_two(lines):
     '''
     '''
 
-    pass
+    bags_dictionary = {}
+
+    for line in lines:
+        first_capture = re.match(r'(.+) bags contain (.+)\.$', line)
+        parent_bag = first_capture.group(1)
+        contents = first_capture.group(2).split(', ')
+        for bag in contents:
+            second_capture = re.match(r'(\d+) (.+) bags?', bag)
+            inner_bags = bags_dictionary.get(parent_bag, {})
+            if second_capture:
+                inner_bag = second_capture.group(2)
+                bag_count = second_capture.group(1)
+                inner_bags[inner_bag] = bag_count
+
+            bags_dictionary[parent_bag] = inner_bags
+
+    return recursive_bag_total(bags_dictionary, 'shiny gold') - 1
+
+def recursive_bag_total(dictionary, key):
+    '''
+    '''
+
+    total_count = 0
+    sub_bags = dictionary[key]
+
+    for bag, count in sub_bags.items():
+        total_count += int(count) * recursive_bag_total(dictionary, bag)
+        #total_count += int(count)
+
+    total_count += 1
+
+    return total_count
