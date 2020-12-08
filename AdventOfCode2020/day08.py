3dc558f26722b90118ea08982e350461374ca0d8
Lachlan Kerr <lachlankerr96@gmail.com>
2020-12-09 09:22:43 +1100

    Finish day 08

--- AdventOfCode2020/day08.py
+++ AdventOfCode2020/day08.py
@@ -17,9 +17,8 @@ def read_input():
 
 def part_one(lines):
     '''
+        Gets the value of the accumulator when an instruction repeats itself
     '''
-
-    instructions = ['acc', 'jmp', 'nop']
 
     instruction_history = {}
 
@@ -48,6 +47,47 @@ def part_one(lines):
 
 def part_two(lines):
     '''
+        Attempts to fix a broken instruction list by swapping exactly one nop -> jmp or jmp -> nop
     '''
 
-    pass
+    accumulator = 0
+
+    no_loops = False
+
+    swapped_instructions = []
+
+    while not no_loops:
+        no_loops = True
+        swap_next_nop_or_jmp = True
+        instruction_history = {}
+        accumulator = 0
+        i = 0
+        while i < len(lines):
+            instruction_history[i] = instruction_history.get(i, 0) + 1
+            if instruction_history[i] > 1:
+                no_loops = False
+                break
+            capture = re.match(r'(\w+) ([+-])(\d+)', lines[i])
+            instruction = capture.group(1)
+            sign = capture.group(2)
+            value = capture.group(3)
+            number = int(sign + value)
+            if instruction == 'acc':
+                accumulator += number
+            elif instruction == 'jmp':
+                if swap_next_nop_or_jmp and i not in swapped_instructions:
+                    swap_next_nop_or_jmp = False
+                    swapped_instructions.append(i)
+                else:
+                    i += number
+                    continue
+            elif instruction == 'nop':
+                if swap_next_nop_or_jmp and i not in swapped_instructions:
+                    swap_next_nop_or_jmp = False
+                    swapped_instructions.append(i)
+                    i += number
+                    continue
+            i += 1
+
+
+    return accumulator
