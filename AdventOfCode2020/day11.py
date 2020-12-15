b71f12a9770748f651026fa60bfb6ed7999085e2
Lachlan Kerr <lachlankerr96@gmail.com>
2020-12-15 11:06:02 +1100

    Speeding up day 11 part 1

--- AdventOfCode2020/day11.py
+++ AdventOfCode2020/day11.py
@@ -2,6 +2,20 @@
     Day 11: https://adventofcode.com/2020/day/11
 '''
 
+height = 0
+width = 0
+
+positions_to_check = [
+    [-1, -1],
+    [-1, 0],
+    [-1, 1],
+    [0, -1],
+    [0, 1],
+    [1, -1],
+    [1, 0],
+    [1, 1]
+]
+
 def read_input():
     '''
         Reads the input file and returns a list of all the lines
@@ -16,8 +30,13 @@ def read_input():
 def part_one(lines):
     '''
     '''
+    global height
+    global width
 
     seats_changed = 1
+
+    height = len(lines)
+    width = len(lines[0])
 
     while seats_changed != 0:
         lines, seats_changed = apply_rules(lines)
@@ -44,15 +63,17 @@ def apply_rules(lines):
     seats_changed = 0
 
     row = 0
-    while row < len(lines):
+    while row < height:
         col = 0
-        while col < len(lines[row]):
-            if should_be_occupied(lines, row, col):
-                #print(result[row])
+        while col < width:
+            if lines[row][col] == '.':
+                col += 1
+                continue
+            occupied = adjacent_occupied(lines, row, col)
+            if lines[row][col] == 'L' and occupied == 0:
                 result[row] = result[row][:col] + '#' + result[row][col + 1:]
-                #print(result[row])
                 seats_changed += 1
-            if should_be_empty(lines, row, col):
+            elif lines[row][col] == '#' and occupied >= 4:
                 result[row] = result[row][:col] + 'L' + result[row][col + 1:]
                 seats_changed += 1
             col += 1
@@ -60,45 +81,38 @@ def apply_rules(lines):
 
     return (result, seats_changed)
 
-def should_be_occupied(lines, row, col):
+def adjacent_occupied(lines, row, col):
     '''
     '''
 
     occupied_seats = 0
 
-    if lines[row][col] == 'L':
-        i = row - 1 if row > 0 else 0
-        while i <= row + 1 and i < len(lines):
-            j = col - 1 if col > 0 else 0
-            while j <= col + 1 and j < len(lines[row]):
-                if not (i == row and j == col):
-                    if lines[i][j] == '#':
-                        occupied_seats += 1
-                j += 1
-            i += 1
-    else:
-        return False
+    for position in positions_to_check:
+        x = row + position[0]
+        y = col + position[1]
 
-    return occupied_seats == 0
+        if x < 0 or y < 0 or x >= height or y >= width:
+            continue
 
-def should_be_empty(lines, row, col):
+        if lines[x][y] == '#':
+            occupied_seats += 1
+
+    return occupied_seats
+
+def adjacent_occupied_slow(lines, row, col):
     '''
     '''
 
     occupied_seats = 0
 
-    if lines[row][col] == '#':
-        i = row - 1 if row > 0 else 0
-        while i <= row + 1 and i < len(lines):
-            j = col - 1 if col > 0 else 0
-            while j <= col + 1 and j < len(lines[row]):
-                if not (i == row and j == col):
-                    blah = lines[i][j]
-                    if lines[i][j] == '#':
-                        occupied_seats += 1
-                j += 1
-            i += 1
-    else:
-        return False
+    i = row - 1 if row > 0 else 0
+    while i <= row + 1 and i < height:
+        j = col - 1 if col > 0 else 0
+        while j <= col + 1 and j < width:
+            if not (i == row and j == col): # must skip current seat
+                if lines[i][j] == '#':
+                    occupied_seats += 1
+            j += 1
+        i += 1
 
-    return occupied_seats >= 4
+    return occupied_seats
